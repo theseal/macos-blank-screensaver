3d9c4590b169ab7ce88008bacee962cc1a7d2b43
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2018-10-05 10:44:59 +0200

    add Blank xcode project

---
+++ Blank/Blank/BlankView.h
@@ -0,0 +1,13 @@
+//
+//  BlankView.h
+//  Blank
+//
+//  Created by Gustav on 2018-10-05.
+//  Copyright © 2018 Rymdvarelse. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BlankView : ScreenSaverView
+
+@end
